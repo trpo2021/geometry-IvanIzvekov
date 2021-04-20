219544eceabf59aec54f1090a557154902311471
Ivan Izvekov <ivan.izvekov02@mail.ru>
2021-04-20 17:40:46 +0700

    Add new function

--- src/geometry/geometry.cpp
+++ src/geometry/geometry.cpp
@@ -1,18 +1,33 @@
-#include "libgeometry/inputProcessing.h"
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include "libgeometry/inputProcessing.h"
+#include "libgeometry/Circle.h"
+
 
 using namespace std;
 
+
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
 
-    string input;
-    while (true) {
-        getline(cin, input, '\n');
-        processing(input);
-        cout << endl;
-    }
+	vector<Circle> circles;
 
-    return 0;
+	string input;
+	while (true) {
+		getline(cin, input, '\n');
+		if (processing(circles, input)) {
+			cout << "Пересечения:" << endl;
+			Circle ncircle = circles.back();
+                for (unsigned int i = 0; i < circles.size() - 1; i++) {
+				if (circles[i].radius + ncircle.radius > sqrt(pow(circles[i].x - ncircle.x, 2) + pow(circles[i].y - ncircle.y, 2))) {
+					cout << "  circle(" << circles[i].x << " " << circles[i].y << ", " << circles[i].radius << ")" << endl;
+				}
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
--- src/libgeometry/inputProcessing.h
+++ src/libgeometry/inputProcessing.h
@@ -2,25 +2,18 @@
 
 #include <string>
 #include <vector>
+#include "Circle.h"
+
 
 using std::string;
 using std::vector;
 
-void processing(string command);
+bool processing(vector<Circle>& circles, string command);
 void normalization(string& command);
 
 vector<string> splitting(string& command);
 
 bool numberParser(string& strNumber, double& number);
 
-void parser(vector<string>& details);
-void circleParser(
-        vector<string>& details, double& x, double& y, double& radius);
-void triangleParser(
-        vector<string>& details,
-        double& x1,
-        double& y1,
-        double& x2,
-        double& y2,
-        double& x3,
-        double& y3);
+bool parser(vector<Circle>& circles, vector<string>& details);
+bool circleParser(vector<string>& details, double& x, double& y, double& radius);
