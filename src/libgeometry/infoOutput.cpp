b3fcfa7899eacf9e8e1b669827438ec935e610f8
Ivan Izvekov <ivan.izvekov02@mail.ru>
2021-04-25 18:40:03 +0700

    Add full test

--- src/libgeometry/infoOutput.cpp
+++ src/libgeometry/infoOutput.cpp
@@ -1,10 +1,10 @@
 #include "infoOutput.h"
 #include <iostream>
+#include <cmath>
 
 
 using std::cout;
 using std::endl;
-
 
 const double PI = 3.141592653589793238463;
 
@@ -15,6 +15,20 @@ void circleInfo(double x, double y, double radius)
 	cout << "y = " << y << endl;
 
 	cout << "radius = " << radius << endl;
-	cout << "P = " << 2 * PI * radius << endl;
-	cout << "S = " << PI * radius * radius << endl;
+	cout << "P = " << circlePerimetr(radius) << endl;
+	cout << "S = " << circleArea(radius) << endl;
+	
+}
+double circleArea(double radius){
+	return round(PI * radius * radius*1000)/1000;
+}
+double circlePerimetr(double radius){
+	return round(2 * PI * radius*1000)/1000;
+}
+bool intersection (const Circle& A,const Circle& B)
+{
+	if (A.radius + B.radius > sqrt(pow(A.x - B.x, 2) + pow(A.y - B.y, 2))) {
+					return true;
+				}
+				return false;
 }
